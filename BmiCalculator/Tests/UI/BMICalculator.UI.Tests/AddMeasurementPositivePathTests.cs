a0f9eef488ee6b7725fa634a65054597e04abc60
Leszek Lewandowski <leszek.lewandowski@dnv.com>
2024-02-28 12:49:08 +0100

    Negative path

--- BmiCalculator/Tests/UI/BMICalculator.UI.Tests/AddMeasurementPositivePathTests.cs
+++ BmiCalculator/Tests/UI/BMICalculator.UI.Tests/AddMeasurementPositivePathTests.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 
 namespace BMICalculator.UI.Tests
 {
@@ -31,6 +32,30 @@ namespace BMICalculator.UI.Tests
             var results = driver.FindElements(By.XPath("//b[contains(.,\'30.86\')]"));
 
             Assert.True(results.Count > 0);
+        }
+
+        [Test]
+        public void AddMeasurementNegativePath()
+        {
+            driver.Navigate().GoToUrl("https://localhost:7250/");
+            driver.Manage().Window.Size = new System.Drawing.Size(1920, 1050);
+            {
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+                wait.Until(driver => driver.FindElements(By.CssSelector(".display-4")).Count > 0);
+            }
+            driver.FindElement(By.LinkText("Pomiar BMI")).Click();
+            driver.FindElement(By.Id("Weight")).SendKeys("1000");
+            driver.FindElement(By.Id("Height")).SendKeys("1000");
+            {
+                var dropdown = driver.FindElement(By.CssSelector(".form-select"));
+                dropdown.FindElement(By.XPath("//option[. = 'Imperialny']")).Click();
+            }
+            driver.FindElement(By.CssSelector(".btn-primary")).Click();
+            var elements = driver.FindElements(By.XPath("//span[contains(.,\'The field Weight must be between 1 and 400.\')]"));
+            Assert.True(elements.Count > 0);
+
+            elements = driver.FindElements(By.XPath("//span[contains(.,\'The field Height must be between 1 and 250.\')]"));
+            Assert.True(elements.Count > 0);
         }
     }
 }
