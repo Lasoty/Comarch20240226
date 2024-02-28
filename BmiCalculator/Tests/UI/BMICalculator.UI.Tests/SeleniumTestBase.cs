7c738f439d69ed829eeecd6facaef35ef87ad327
Leszek Lewandowski <leszek.lewandowski@dnv.com>
2024-02-28 10:43:00 +0100

    Konfiguracja Selenium

---
+++ BmiCalculator/Tests/UI/BMICalculator.UI.Tests/SeleniumTestBase.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace BMICalculator.UI.Tests
+{
+    public abstract class SeleniumTestBase
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--disable-extensions");
+            options.AddArgument("--disable-gpu");
+            //options.AddArgument("--headless=new");
+
+            driver = new ChromeDriver();
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Close();
+            driver.Quit();
+            driver.Dispose();
+        }
+    }
+}
